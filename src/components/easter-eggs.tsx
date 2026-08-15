2bf0fc8aa00859d1738005b55cf44f4203365b82
yashpatgavkar <yashpatgavkar1@gmail.com>
2026-08-15 06:06:07 +0530

    Update paint stroke easter egg hint

--- src/components/easter-eggs.tsx
+++ src/components/easter-eggs.tsx
@@ -46,7 +46,7 @@ const EasterEggs = () => {
             const timer = setTimeout(() => {
               console.log(
                 "%cPssttt! 🤫\n\n" +
-                  "Do you like cats?? 😺 If yes, then press 'n' on viewport and see what happens! 🐱✨",
+                  "Press 'P' on the page to send a paint stroke across the screen! 🎨✨",
                 "color: #FF69B4; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
               );
               clearTimeout(timer);
